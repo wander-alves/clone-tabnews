6a367522c46067455996461b79e8286c19ade89a
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2025-06-25 23:21:49 -0300

    adicionando o arquivo de layout para poder trabalhar com estilos

---
+++ pages/_app.js
@@ -0,0 +1,22 @@
+import Head from "next/head";
+
+import './style.css'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>My place on the universe</title>
+      </Head>
+      <main>
+        <div className="content-container">
+          <img src='/marvin-without-background.png' alt='Marvin' />
+          <div>
+            <h1>Life! <br /> Don't talk to me about life!</h1>
+            <em><q>Marvin, the Paranoid Android</q></em>
+          </div>
+        </div>
+      </main>
+    </>
+  )
+}
