5e1229dcd77e5216ff88fc594608e96ebccc9897
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2025-09-27 02:27:09 -0300

    adicionando a validação de CA nas configurações do banco

--- infra/database.js
+++ infra/database.js
@@ -7,7 +7,7 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env === "development" ? false : true,
+    ssl: getSSLValues(),
   });
   await client.connect();
 
@@ -24,3 +24,12 @@ async function query(queryObject) {
 export default {
   query,
 };
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+  return process.env.NODE_ENV === "development" ? false : true;
+}
