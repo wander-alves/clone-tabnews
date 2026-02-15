c044a9ab49f7606476bb3d163c6450e4a69df91f
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2026-02-14 23:41:24 -0300

    feat: implement `ValidationError` in `controller.js` module

--- infra/errors.js
+++ infra/errors.js
@@ -38,6 +38,27 @@ class ServiceError extends Error {
   }
 }
 
+class ValidationError extends Error {
+  constructor({ cause, message, action }) {
+    super(message || "Um erro de validação ocorreu.", {
+      cause,
+    });
+    this.name = "ValidationError";
+    this.action =
+      action || "Por favor, verifique os dados enviados e tente novamente.";
+    this.statusCode = 400;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
 class MethodNotAllowedError extends Error {
   constructor() {
     super("O método utilizado não é suportado pelo endpoint.");
@@ -56,4 +77,9 @@ class MethodNotAllowedError extends Error {
   }
 }
 
-export { InternalServerError, MethodNotAllowedError, ServiceError };
+export {
+  InternalServerError,
+  MethodNotAllowedError,
+  ServiceError,
+  ValidationError,
+};
