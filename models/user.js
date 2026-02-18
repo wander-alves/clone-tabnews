1600aa50c6ca9556d1f42b4aea398206d1f79a14
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2026-02-18 01:29:04 -0300

    feat: add endpoint `GET /users/[username]` and test coverage

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 import database from "infra/database.js";
-import { ValidationError } from "infra/errors.js";
+import { ValidationError, NotFoundError } from "infra/errors.js";
 
 async function create({ username, email, password }) {
   await validateUniqueEmail(email);
@@ -65,8 +65,40 @@ async function create({ username, email, password }) {
   }
 }
 
+async function findOneByUsername(username) {
+  const newUser = await runSelectQuery(username);
+
+  return newUser;
+
+  async function runSelectQuery(username) {
+    const result = await database.query({
+      text: `
+        SELECT
+          *
+        FROM 
+          users
+        WHERE
+          LOWER(username) = LOWER($1)
+        LIMIT
+          1
+      ;`,
+      values: [username],
+    });
+
+    if (result.rowCount === 0) {
+      throw new NotFoundError({
+        message: "Não foi possível localizar o usuário informado.",
+        action: "Verifique o username informado e tente novamente.",
+      });
+    }
+
+    return result.rows[0];
+  }
+}
+
 const user = {
   create,
+  findOneByUsername,
 };
 
 export default user;
