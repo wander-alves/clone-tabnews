aaa32c6bb760a6f7e1044b523d409421a6b28017
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2026-04-21 19:01:35 -0300

    refactor: rename `authorization` method `getAuthenticatedUser()` to `getUser()`

--- pages/api/v1/sessions/index.js
+++ pages/api/v1/sessions/index.js
@@ -17,7 +17,7 @@ router.delete(deleteHandler);
 async function postHandler(request, response) {
   const userInputValues = request.body;
 
-  const authenticatedUser = await authentication.getAuthenticatedUser(
+  const authenticatedUser = await authentication.getUser(
     userInputValues.email,
     userInputValues.password,
   );
