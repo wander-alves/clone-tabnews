539e84799f28255745feb88c8643c083986bc3b8
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2026-04-21 20:16:17 -0300

    feat: add `sameSite=Lax` to `controller.setSessionCookie()`

--- tests/integration/api/v1/user/get.test.js
+++ tests/integration/api/v1/user/get.test.js
@@ -84,6 +84,7 @@ describe("[GET] /api/v1/user", () => {
         path: "/",
         maxAge: session.EXPIRATION_IN_MILLISECONDS / 1000,
         httpOnly: true,
+        sameSite: "Lax",
       });
     });
 
@@ -155,6 +156,7 @@ describe("[GET] /api/v1/user", () => {
         path: "/",
         maxAge: session.EXPIRATION_IN_MILLISECONDS / 1000,
         httpOnly: true,
+        sameSite: "Lax",
       });
     });
 
