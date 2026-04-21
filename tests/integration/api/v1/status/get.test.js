0f10724fa99c62b9748ce9d6fa780e2788fc087b
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2026-04-21 20:10:37 -0300

    test: add default user coverage in `GET /api/v1/status` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -29,6 +29,31 @@ describe("[GET] /api/v1/status", () => {
     });
   });
 
+  describe("Default user", () => {
+    test("it should return current endpoint status without `version` information when requested", async () => {
+      const createdUser = await orchestrator.createUser();
+      await orchestrator.activateUserByUserId(createdUser.id);
+      const userSession = await orchestrator.createSession(createdUser.id);
+
+      const response = await fetch(`${webserver.origin}/api/v1/status`, {
+        headers: {
+          Cookie: `session_id=${userSession.token}`,
+        },
+      });
+
+      expect(response.status).toBe(200);
+
+      const responseBody = await response.json();
+      const parsedDate = new Date(responseBody.updated_at).toISOString();
+
+      expect(responseBody.updated_at).toEqual(parsedDate);
+
+      expect(responseBody.dependencies.database.max_connections).toEqual(100);
+      expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+      expect(responseBody.dependencies.database).not.toHaveProperty("version");
+    });
+  });
+
   describe("Privileged user", () => {
     test("it should return current endpoint status when requested", async () => {
       const createdUser = await orchestrator.createUser();
